04b95dbd1d317acffd882afdea49a66a6eb9d36a
Lucas Garzeri de Melo <97560175+LucasGarzerideMelo@users.noreply.github.com>
2022-07-13 14:39:47 -0300

    Create Multiplicador.py

---
+++ Multiplicador.py
@@ -0,0 +1,9 @@
+def mult(a , b):
+  return a * b
+
+n1 = int(input("Digite um numero :"))
+n2 = int(input("Digite o multiplicador desse numero :"))
+
+multiplicacao = mult(n1 , n2)
+
+print("O produto entre esses numeros e: {}".format(multiplicacao))
