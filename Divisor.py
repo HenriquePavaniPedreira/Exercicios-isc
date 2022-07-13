f9ab2e0432e6a656e6c2bfc2db563b66342ef823
Lucas Garzeri de Melo <97560175+LucasGarzerideMelo@users.noreply.github.com>
2022-07-13 14:43:42 -0300

    Create Divisor.py

---
+++ Divisor.py
@@ -0,0 +1,9 @@
+def div(a , b):
+  return a / b
+
+n1 = int(input("Digite um numero :"))
+n2 = int(input("Digite um divisor para esse numero :"))
+
+divisao = div(n1 , n2)
+
+print("O quociente da divisao e: {}".format(divisao))
